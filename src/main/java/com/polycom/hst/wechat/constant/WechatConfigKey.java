bb59860a12379a757742cd57802725c288b1d051
snake0zero <snake0zero@hotmail.com>
2016-01-22 14:31:56 +0800

    update

--- src/main/java/com/polycom/hst/wechat/constant/WechatConfigKey.java
+++ src/main/java/com/polycom/hst/wechat/constant/WechatConfigKey.java
@@ -5,4 +5,6 @@ public class WechatConfigKey {
 	public static String WECHAT_APPID = "wechat.appid";
 	public static String WECHAT_APPSECRET = "wechat.appsecret";
 	public static String WECHAT_REDIRECT_DOMAIN = "wechat.redirectdomain";
+	public static String WECHAT_OPEN_APPID = "wechat.open.appid";
+	public static String WECHAT_OPEN_APPSECRET = "wechat.open.appsecret";
 }
